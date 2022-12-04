06ad1aaeac3bcdf09e35eb63fc61957685ea598f
Syed Osama Ahmed <batch17.026@gmail.com>
2022-12-04 20:31:44 +0500

    created user with email and now moving firebase initialization before app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_Options.dart';
 
 void main() {
   runApp(const MyApp());
@@ -62,11 +65,32 @@ class _HomePageState extends State<HomePage> {
         children: [
           TextField(
             controller: _email,
+            enableSuggestions: false,
+            autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(hintText: 'Enter email'),
           ),
           TextField(
             controller: _password,
+            obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: const InputDecoration(hintText: 'Enter password'),
           ),
-          TextButton(onPressed: () {}, child: (const Text('Register'))),
+          TextButton(
+              onPressed: () async {
+                await Firebase.initializeApp(
+                  options: DefaultFirebaseOptions.currentPlatform,
+                );
+                final email = _email.text;
+                final password = _password.text;
+
+                final userCredentials = await FirebaseAuth.instance
+                    .createUserWithEmailAndPassword(
+                        email: email, password: password);
+                print(userCredentials);
+              },
+              child: (const Text('Register'))),
         ],
       ),
     );

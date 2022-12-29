bc0d4bd3501409da012be2ed909d9086d306ac84
Syed Osama Ahmed <batch17.026@gmail.com>
2022-12-29 15:17:23 +0500

    completing main ui of counter app using bloc in progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:learning_project/constants/routes.dart';
 
-import 'package:learning_project/services/auth/auth_service.dart';
+// import 'package:learning_project/services/auth/auth_service.dart';
 import 'package:learning_project/views/login_view.dart';
 import 'package:learning_project/notes/notes_view.dart';
 
@@ -49,7 +50,7 @@ class MyApp extends StatelessWidget {
 
 //stateless widget: cannot redraw itself and contain mutable data.
 
-class HomePage extends StatelessWidget {
+/* class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
@@ -86,6 +87,129 @@ class HomePage extends StatelessWidget {
                 ],
               ),
             ));
+        }
+      },
+    );
+  }
+}
+ */
+
+//bloc implementation:
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final TextEditingController _controller;
+
+  @override
+  void initState() {
+    _controller = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+        create: (context) => CounterBloc(),
+        child: Scaffold(
+          appBar: AppBar(title: const Text('Testing bloc')),
+          body: BlocConsumer<CounterBloc, CounterState>(
+            listener: (context, state) {
+              _controller.clear();
+            },
+            builder: (context, state) {
+              final invalidVal =
+                  (state is CounterStateInvalid) ? state.invalidValue : '';
+
+              return Column(
+                children: [
+                  Text('state value is ${state.value}'),
+                  Visibility(
+                    visible: state is CounterStateInvalid,
+                    child: Text('invalid input $invalidVal'),
+                  ),
+                  TextField(
+                    controller: _controller,
+                    decoration: const InputDecoration(
+                      hintText: 'Enter a number',
+                    ),
+                    keyboardType: TextInputType.number,
+                  )
+                ],
+              );
+            },
+          ),
+        ));
+  }
+}
+
+@immutable
+abstract class CounterState {
+  final int value;
+  const CounterState(this.value);
+}
+
+class CounterStateValid extends CounterState {
+  const CounterStateValid(int value) : super(value);
+}
+
+class CounterStateInvalid extends CounterState {
+  final String invalidValue;
+  const CounterStateInvalid(
+      {required this.invalidValue, required int previousValue})
+      : super(previousValue);
+}
+
+@immutable
+abstract class CounterEvent {
+  final String value;
+  const CounterEvent(this.value);
+}
+
+class IncrementEvent extends CounterEvent {
+  const IncrementEvent(String value) : super(value);
+}
+
+class DecrementEvent extends CounterEvent {
+  const DecrementEvent(String value) : super(value);
+}
+
+class CounterBloc extends Bloc<CounterEvent, CounterState> {
+  CounterBloc() : super(const CounterStateValid(0)) {
+    on<IncrementEvent>(
+      (event, emit) {
+        final integer = int.tryParse(event.value);
+        if (integer == null) {
+          emit(CounterStateInvalid(
+            invalidValue: event.value,
+            previousValue: state.value,
+          ));
+        } else {
+          emit(CounterStateValid(state.value + integer));
+        }
+      },
+    );
+    on<DecrementEvent>(
+      (event, emit) {
+        final integer = int.tryParse(event.value);
+        if (integer == null) {
+          emit(CounterStateInvalid(
+            invalidValue: event.value,
+            previousValue: state.value,
+          ));
+        } else {
+          emit(CounterStateValid(state.value - integer));
         }
       },
     );
